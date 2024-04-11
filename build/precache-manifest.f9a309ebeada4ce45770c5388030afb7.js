257c6b7d7a1f5c540d3a5e345fe13af00f5c67a6
Benjamin <Benjaminkakai001@gmail.com>
2024-04-11 18:26:12 +0300

    Yup

---
+++ build/precache-manifest.f9a309ebeada4ce45770c5388030afb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c627efba95735e22f685d3f3b19092c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5acac0e8f44b6882878",
+    "url": "/static/css/main.a4a36736.chunk.css"
+  },
+  {
+    "revision": "1651e4707f2a3bd82497",
+    "url": "/static/js/2.c5a4d13d.chunk.js"
+  },
+  {
+    "revision": "132114737367cc2cbf26af9c5a0d4f77",
+    "url": "/static/js/2.c5a4d13d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c6e190501741c5f627",
+    "url": "/static/js/3.52eed761.chunk.js"
+  },
+  {
+    "revision": "c5acac0e8f44b6882878",
+    "url": "/static/js/main.6fcfbd82.chunk.js"
+  },
+  {
+    "revision": "f78c94197d69216e4160",
+    "url": "/static/js/runtime-main.cf5ebd3b.js"
+  }
+]);
